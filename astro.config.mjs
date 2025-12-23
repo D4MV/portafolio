24a547760e08d38563150cdd170dcbf27bdfc8eb
D4MV <diego.moragavaldes2002@gmail.com>
2025-12-23 15:42:14 -0300

    subiendo portafolio 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,7 @@ import * as consts from "./src/consts.ts";
 
 export default defineConfig({
   site: /** @type {any} */ (consts).SITE_URL ?? "http://localhost:3000",
-  // Descomenta la siguiente línea si tu repositorio se llama diferente a tu-usuario.github.io
-  // base: '/nombre-de-tu-repositorio',
+  base: "/portafolio",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
